ce225dd23515dc8c3ed48539317bcb2cad844adb
Jose Luis <crasho1481@gmail.com>
2015-08-21 18:08:37 -0400

    Added My First Applications named banks and my First template

--- multinivel_django/settings.py
+++ multinivel_django/settings.py
@@ -82,6 +82,17 @@ DATABASES = {
 }
 
 
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(
+        os.path.dirname(__file__),
+        'static',
+    ),
+)
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
